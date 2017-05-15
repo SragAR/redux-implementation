ff9292189c269a5f447782e6dff483c71ec185c1
cyrilc@qburst.com <cyrilc@qburst.com>
2017-05-15 10:25:04 +0530

    basic redux flow

--- src/index.js
+++ src/index.js
@@ -6,13 +6,21 @@ const initialState = {
     visibilityFilter: VisibilityFilters.SHOW_ALL,
     todos: []
 }
+
 let store = createStore(todoApp , initialState); // initial state is optional
 console.log('initial ',store.getState()); //prints initial state
 
-store.subscribe(() =>
+//logs all state change
+let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
 )
 
 store.dispatch(addTodo('first textttttttttttttttttttttttt'));
 store.dispatch(toggleTodo(0));
 store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+// Stop listening to state updates
+ unsubscribe();
+
+
+
